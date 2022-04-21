74773dccf1c7450a82670b32081a843eae93f2fa
J4MSH1D <jamshidabdukadirov01@gmail.com>
2022-04-21 15:14:19 +0500

    created login form fot google auth

--- src/components/Auth/Login.vue
+++ src/components/Auth/Login.vue
@@ -1,3 +1,12 @@
 <template>
-    <div class="login w-1/2 py-10 bg-white z-20 relative"></div>
+  <div
+    class="flex text-white item-center justify-center flex-col login md:w-1/3 w-2/3 z-20 relative"
+  >
+    <img
+      src="../../assets/images/Epic_Games_logo.svg"
+      class="md:h-40 h-32"
+      alt="logo"
+    />
+    <button class="w-full font-semibold transition hover:bg-blue-600 active:bg-blue-800 bg-blue-700 rounded-lg md:py-3 py-2 text-lg text-center mt-10">Get All There</button>
+  </div>
 </template>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -6,12 +6,12 @@ import { RouterLink } from "vue-router";
     class="flex justify-between items-center text-white z-30 px-10 w-screen py-2 fixed top-0 left-0 bg-slate-900"
   >
     <div class="cursor-pointer">
-			<img class="h-10" src="../assets/images/Logo.png" alt="logo">
-		</div>
+      <img class="h-10" src="../assets/images/Logo.png" alt="logo" />
+    </div>
     <div class="font-semibold text-lg">
       <RouterLink to="/home">Home</RouterLink>
-      <RouterLink class="mx-5" to="/">Login</RouterLink>
-      <span class="cursor-pointer">log Out</span>
+      <RouterLink class="mx-5 border rounded px-2 py-1 transition hover:text-slate-900 hover:bg-white " to="/">Login</RouterLink>
+      <span class="cursor-pointer border rounded px-2 py-1 transition hover:text-slate-900 hover:bg-white">Log Out</span>
     </div>
   </div>
 </template>
